cd7d1999f940944e9d840ca76c02da5b62389382
younki.cho <gloss62@naver.com>
2018-08-07 16:31:46 +0900

    Test.java 첫줄 주석

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String [] args){
-		System.out.println("Heo");
+		//System.out.println("Heo");
 		System.out.println("Git");
 		System.out.println("Hi Git");
 		System.out.println("branch1");
